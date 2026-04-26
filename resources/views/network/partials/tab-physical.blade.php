7cc22124edbb32f91bf10673f6993885f3bd9fc0
Chavez <L202307013@vbravo.tecnm.mx>
2026-04-25 18:53:12 -0600

    Cosas de interfaz

--- resources/views/network/partials/tab-physical.blade.php
+++ resources/views/network/partials/tab-physical.blade.php
@@ -1,20 +1,21 @@
 <div class="row mb-4">
     <div class="col-md-3 text-md-end pt-1">
-        <label class="form-label mb-0"
-            style="color: var(--text-main); font-weight: 600; font-size: 0.9rem;">Puentear interfaces</label>
+        <label class="form-label mb-0" style="color: var(--text-main); font-weight: 600; font-size: 0.9rem;">Puentear
+            interfaces</label>
     </div>
     <div class="col-md-9">
         <div class="form-check m-0 mb-1">
             <input class="form-check-input" type="checkbox" name="type" id="editIfaceType" value="bridge">
         </div>
-        <small style="color: var(--text-muted); font-size: 0.75rem;">Crea un puente sobre la interfaz o interfaces asociadas (br-lan)</small>
+        <small style="color: var(--text-muted); font-size: 0.75rem;">Crea un puente sobre la interfaz o interfaces
+            asociadas (br-lan)</small>
     </div>
 </div>
 
 <div class="row align-items-start mb-4">
     <div class="col-md-3 text-md-end pt-2">
-        <label class="form-label mb-0"
-            style="color: var(--text-main); font-weight: 600; font-size: 0.9rem;">Dispositivo Físico</label>
+        <label class="form-label mb-0" style="color: var(--text-main); font-weight: 600; font-size: 0.9rem;">Dispositivo
+            Físico</label>
     </div>
     <div class="col-md-9">
         <!-- Render a list of options using $devices from controller -->
@@ -26,7 +27,8 @@
             @endif
             <option value="custom">-- Personalizado --</option>
         </select>
-        <input type="text" name="custom_ifname" id="editIfaceIfnameCustom" class="form-control w-75 mt-2 d-none" placeholder="Ej: eth0.3">
+        <input type="text" name="custom_ifname" id="editIfaceIfnameCustom" class="form-control w-75 mt-2 d-none"
+            placeholder="Ej: eth0.3">
         <small class="d-block mt-2" style="color: var(--text-muted); font-size: 0.75rem;">
             (Para seleccionar múltiples mantenga presionada la tecla Ctrl/Cmd)
         </small>
